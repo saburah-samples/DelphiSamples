ce39239105cd535437e8772df6de0b936baf65d2
saburah <saburah2@gmail.com>
2015-04-12 09:33:05 +0400

    sort person mapping handlers

--- Mapping/PersonMapping.pas
+++ Mapping/PersonMapping.pas
@@ -9,26 +9,6 @@ uses
   Mapping,
   PersonModule;
 
-procedure AddressMapperHandler(ASource, ATarget: TAddress);
-begin
-  ATarget.Region := ASource.Region;
-  ATarget.City := ASource.City;
-  ATarget.Street := ASource.Street;
-end;
-
-procedure PersonMapperHandler(ASource, ATarget: TPerson);
-begin
-  ATarget.Name := ASource.Name;
-  ATarget.Address := ASource.Address;
-  ATarget.Contacts := ASource.Contacts;
-end;
-
-procedure ContactMapperHandler(ASource, ATarget: TContact);
-begin
-  ATarget.Kind := ASource.Kind;
-  ATarget.Value := ASource.Value;
-end;
-
 procedure ListMapperHandler(ASource, ATarget: TList);
 var
   I: Integer;
@@ -42,10 +22,30 @@ begin
   end;
 end;
 
+procedure AddressMapperHandler(ASource, ATarget: TAddress);
+begin
+  ATarget.Region := ASource.Region;
+  ATarget.City := ASource.City;
+  ATarget.Street := ASource.Street;
+end;
+
+procedure ContactMapperHandler(ASource, ATarget: TContact);
+begin
+  ATarget.Kind := ASource.Kind;
+  ATarget.Value := ASource.Value;
+end;
+
+procedure PersonMapperHandler(ASource, ATarget: TPerson);
+begin
+  ATarget.Name := ASource.Name;
+  ATarget.Address := ASource.Address;
+  ATarget.Contacts := ASource.Contacts;
+end;
+
 initialization
-  MapperManager.RegisterMapper(TAddress, TAddress, @AddressMapperHandler);
-  MapperManager.RegisterMapper(TPerson, TPerson, @PersonMapperHandler);
-  MapperManager.RegisterMapper(TContact, TContact, @ContactMapperHandler);
   MapperManager.RegisterMapper(TList, TList, @ListMapperHandler);
+  MapperManager.RegisterMapper(TAddress, TAddress, @AddressMapperHandler);
+  MapperManager.RegisterMapper(TContact, TContact, @ContactMapperHandler);
+  MapperManager.RegisterMapper(TPerson, TPerson, @PersonMapperHandler);
 
 end.
